0164059a4856c4d693ea8ad5578183c484f327e4
NemethBalintMark <118978756+NemethBalintMark@users.noreply.github.com>
2025-03-12 10:11:40 +0100

    js flip

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 var pont = 0;
 var fruits = ["apple", "cherry", "grape", "orange", "peach", "pear", "strawberry", "watermelon"];
 var cards = [];
+var fordit = 0;
+var forditas = [];
+var pontKiir = document.querySelector(".pont");
 
 function start() {
     pont = 0;
@@ -18,15 +21,50 @@ function start() {
 
     cards = document.querySelectorAll(".kartya");
 
-    console.log(fruits);
+    for (var i = 0; i < cards.length; i++) {
+        cards[i].innerHTML = `<img src="img/card-background.png" alt="Card" title="Card">`;
+        cards[i].classList.remove("flipped");
+    }
+
+    fordit = 0;
+    forditas = [];
+    pontKiir.innerHTML = pont;
 }
 
 function flip(card) {
-    for (var i = 0; i < cards.length; i++) {
-        if (card == cards[i]) {
-            card.innerHTML = `<img src="img/${fruits[i]}.png" alt="Card" title="Card">`;
+    if (!card.classList.contains("flipped") && fordit < 2) {
+        fordit += 1;
+        for (var i = 0; i < cards.length; i++) {
+            if (card == cards[i]) {
+                card.innerHTML = `<img src="img/${fruits[i]}.png" alt="Card" title="Card">`;
+                card.classList.add("flipped");
+
+                forditas.push(fruits[i]);
+            }
+        }
+
+        forditas.push(card);
+
+        if (fordit == 2) {
+            if (forditas[0] != forditas[2]) {
+                setTimeout(() => {
+                    forditas[1].classList.remove("flipped");
+                    forditas[3].classList.remove("flipped");
+                    forditas[1].innerHTML = `<img src="img/card-background.png" alt="Card" title="Card">`;
+                    forditas[3].innerHTML = `<img src="img/card-background.png" alt="Card" title="Card">`;
+                }, 1);
+
+                
+            } else {
+                pont += 1;
+                pontKiir.innerHTML = pont;
+            }
+
+            forditas = [];
+            fordit = 0;
         }
     }
+
 }
 
 start();
